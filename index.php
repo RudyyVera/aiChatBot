2409956e1c4e7fc2eb060900bc8255c105370a29
RudyyWorkSpace <166719770+RudyWorkSpace@users.noreply.github.com>
2025-02-19 04:41:00 -0500

    Subiendo mi chatbot

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Iniciar Sesión</title>
+</head>
+<body>
+    <h1>Iniciar Sesión</h1>
+        Correo Electrónico: <input type="text" name="email"><br>
+        Contraseña: <input type="password" name="password"><br>
+        <a href="index.html">
+        <input type="submit" value="Iniciar Sesión">
+    </a>
+    </form>
+    <?php
+    include("registrar.php");
+    ?>
+</body>
+</html>
+<head>
+    <body>
+
+    </body>
+</head>
